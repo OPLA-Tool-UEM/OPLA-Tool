e75d0afbb99257f46d3f9c8fc694ec482da05cc5
Fernando Godóy <fernandogodoy_18@msn.com>
2017-07-22 23:16:25 -0300

    Adição projeto Opa-Patterns

---
+++ modules/opla-patterns/src/main/java/br/ufpr/dinf/gres/App.java
@@ -0,0 +1,13 @@
+package br.ufpr.dinf.gres;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
