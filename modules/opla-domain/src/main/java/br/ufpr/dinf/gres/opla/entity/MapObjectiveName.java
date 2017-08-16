cccc5271fb91381fed58b7d590edfb1bec1b3dc8
Fernando Godóy <fernandogodoy_18@msn.com>
2017-08-16 20:45:28 -0300

    Criação Camada de Dominio

---
+++ modules/opla-domain/src/main/java/br/ufpr/dinf/gres/opla/entity/MapObjectiveName.java
@@ -0,0 +1,86 @@
+package br.ufpr.dinf.gres.opla.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.util.Objects;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+@Entity
+@Table(name = "map_objectives_names")
+public class MapObjectiveName implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "id", nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "names")
+	private String names;
+
+	@ManyToOne
+	@JoinColumn(name = "experiment_id", nullable = false)
+	private Experiment experiment;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNames() {
+		return names;
+	}
+
+	public void setNames(String names) {
+		this.names = names;
+	}
+
+	public Experiment getExperiment() {
+		return experiment;
+	}
+
+	public void setExperiment(Experiment experiment) {
+		this.experiment = experiment;
+	}
+
+	@Override
+	public boolean equals(final Object other) {
+		if (this == other) {
+			return true;
+		}
+		if (other == null) {
+			return false;
+		}
+		if (!getClass().equals(other.getClass())) {
+			return false;
+		}
+		MapObjectiveName castOther = (MapObjectiveName) other;
+		return Objects.equals(id, castOther.id) && Objects.equals(names, castOther.names)
+				&& Objects.equals(experiment, castOther.experiment);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, names, experiment);
+	}
+
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this, ToStringStyle.SIMPLE_STYLE).append("id", id).append("names", names)
+				.append("experiment", experiment).toString();
+	}
+
+}
